b7eb70350c0ae7a99bf668d7f2621f6d0a7443b3
Lucas Carneiro do Ouro <lusca0320@gmail.com>
2024-09-24 11:52:27 -0300

    Melhorias visuais da home, e correção de bugs

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList, StyleSheet, TouchableOpacity, SafeAreaView, ActivityIndicator } from 'react-native';
+import { View, Text, FlatList, StyleSheet, TouchableOpacity, SafeAreaView, ActivityIndicator, Image } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
@@ -52,17 +52,36 @@ const HomeScreen = ({ navigation }) => {
     setTimeout(() => {
       setLoading(false); 
       navigation.navigate('UserProfile', { login }); 
-    }, 2000); 
+    }, 1000); 
+  };
+
+  const handleLikePress = (postId) => {
+    console.log('Post curtido:', postId);
+    // Lógica futura para curtir a postagem
+  };
+
+  const handleCommentPress = (postId) => {
+    console.log('Comentários para o post:', postId);
+    // Navegar para uma tela ou abrir uma aba de comentários
+    navigation.navigate('Comments', { postId });
   };
 
   const renderPost = ({ item }) => (
-    <TouchableOpacity 
-      style={styles.postContainer}
-      onPress={() => handleUserPress(item.user_login)} 
-    >
-      <Text style={styles.userName}>{item.user_login}</Text>
-      <Text style={styles.postText}>{item.message}</Text>
-    </TouchableOpacity>
+    <View style={styles.postContainer}>
+      <TouchableOpacity onPress={() => handleUserPress(item.user_login)}>
+        <Text style={styles.userName}>{item.user_login}</Text>
+        <Text style={styles.postText}>{item.message}</Text>
+      </TouchableOpacity>
+
+      <View style={styles.iconContainer}>
+        <TouchableOpacity onPress={() => handleLikePress(item.id1)}>
+          <FontAwesome name="heart-o" size={24} color="#ffffff" style={styles.icon} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => handleCommentPress(item.id)}>
+          <FontAwesome name="comment-o" size={24} color="#ffffff" style={styles.icon} />
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 
   return (
@@ -71,6 +90,10 @@ const HomeScreen = ({ navigation }) => {
         <TouchableOpacity onPress={() => navigation.toggleDrawer()}>
           <Text style={styles.headerTitle}>Perfil</Text>
         </TouchableOpacity>
+        <Image
+          style={styles.headerImagem}
+          source={require('../assets/icone.png')}
+        />
         <TouchableOpacity
           style={styles.backButton}
           onPress={() => navigation.navigate('Login')}
@@ -125,15 +148,24 @@ const styles = StyleSheet.create({
     marginTop: 10,
     flex: 1,
   },
+  headerImagem: {
+    width: 30,
+    height: 40,
+    alignSelf: 'center',
+    marginBottom: 5,
+    marginLeft: -10,
+  },
   postContainer: {
     padding: 10,
     backgroundColor: '#1E1E1E',
     borderBottomWidth: 1,
     borderBottomColor: '#333',
+    marginTop: 10, 
   },
   userName: {
     color: '#A9A9A9',
     fontWeight: 'bold',
+    fontSize: 15,
   },
   postText: {
     color: '#ffffff',
@@ -158,6 +190,19 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  iconContainer: {
+    flexDirection: 'row',
+    marginTop: 20,
+    marginLeft: '70%',
+  },
+  icon1: {
+    marginLeft: 10,
+    fontSize: 20,
+  },
+  icon: {
+    marginLeft: 20,
+    fontSize: 20,
   },
 });
 
